9ce8f5e86d72f87f5dbefe4dfc687c2abf0f9a75
Abelsileshie <silesheiabel@gmail.com>
2025-04-12 15:36:53 +0300

    feat: Integrate Zustand for state management and implement login functionality with API integration

--- src/components/chatbot/AiChatbot.jsx
+++ src/components/chatbot/AiChatbot.jsx
@@ -68,7 +68,7 @@ const Chatbot = () => {
   const handleGeminiReply = async (text) => {
     try {
       const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`,
+        `https://backend-bhww.onrender.com/api/chat/message`,
         {
           method: "POST",
           headers: {
