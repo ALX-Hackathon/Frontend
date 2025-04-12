1608eeb2315ff9accdd8578f5aca99c14116450a
Abelsileshie <silesheiabel@gmail.com>
2025-04-12 11:16:27 +0300

    feat: Remove chatbot components and related services

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,4 @@
 // src/config/api.js
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://6497-196-188-78-170.ngrok-free.app";
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://e40e-196-188-78-170.ngrok-free.app/";
